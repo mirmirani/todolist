8b4c4233f1548d09a76660b18e0d5bc283e7a544
Mir Mirani <mir.mirani@outlook.com>
2020-01-29 16:07:03 +0000

    update to list

--- script.js
+++ script.js
@@ -1,149 +1,64 @@
-// script pure js
-var inputBtn = document.getElementById('enter');
-var input = document.getElementById('userInput');
+//script
+var enterButton = document.getElementById("addItem");
+var userInput = document.getElementById("inputBar");
 var ul = document.querySelector("ul");
-var newNote = document.createElement('li');
-var removeIcon = '<i class="fa fa-times"></i>'
-var doneIcon = '<i class="fa fa-check"></i>'
-// variables declared
+var item = document.getElementsByTagName("li");
+// Stable variables
 
- // && event.which === 13)
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(userInput.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	userInput.value = ""; //Reset text input field
 
-function addNote() {
-if (input.value) {
-addnewNote(value);
-input.value = '';
-}}
 
-function addNote2() {
-if (input.value && event.which === 13) {
-'enter'/'return';
-addnewNote(value);
-input.value = '';
-}}
+	//START STRIKETHROUGH
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");
+	}
 
-function removeNote(e) {
-var note = this.parentNode.parentNode;
-var parent = item.parentNode;
+	li.addEventListener("click",crossOut);
+	//END STRIKETHROUGH
 
-parent.removeChild(note);
-}
 
-function completeNote(e) {
-var note = this.parentNode.parentNode;
-var parent = note.parentNode;
-var parentID = parent.id;
+	// START ADD DELETE BUTTON
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+	// END ADD DELETE BUTTON
 
-var target = (id === 'tasks') ? document.getElementById('tasksDone'):document.getElementById('tasks');
 
-parent.removeChild(note);
-target.insertBefore(item, target.childNodes[0]);
-
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
 }
 
 
-function addnewNote() {
-newNote.innerText = text;
-
-var btns = document.createElement('div');
-buttons.classList.add('buttons');
-
-var removeIcon = '<i class="fa fa-times"></i>';
-var doneIcon = '<i class="fa fa-check"></i>';
-
-var remove = document.createElement('button');
-remove.classList.add('remove');
-remove.innerHTML = removeIcon;
-
-remove.addEventListener('click', removeNote);
-
-var complete = document.createElement('button');
-complete.classList.add('complete');
-complete.innerHTML = doneIcon;
-
-complete.addEventListener('click', completeNote);
-complete.classList.add('complete');
-complete.innerHTML = doneIcon;
-
-complete.addEventListener('click', completeNote);
-
-buttons.appendChild(remove);
-buttons.appendChild(complete);
-item.appendChild(buttons);
-
-listenerCount.insertBefore(item, list.childNodes[0]);
+function addListAfterClick(){
+	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
+		createListElement();
+	}
 }
 
-document.getElementById('usierInput').addEventListener('click', addNote)
-document.getElementById('usierInput').addEventListener('keypress', addNote2)
-
-/*
-
-if (inputLength()>0 && event.which === 13) {
-    'enter'/'return' //13 is enter key's code also event.keyCode === 13
-
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
+		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+		createListElement();
+	} 
 }
 
+function inputLength(){
+	return userInput.value.length;
+} 
+
+function listLength(){
+	return item.length;
 }
 
-function crossOut() {
-    li.classList.toggle('done');
-}
+enterButton.addEventListener("click",addListAfterClick);
 
-li.addEventListener('click', crossOut);
-//[end] accomplish note
-//del button
-var dBtn = document.createElement('button');
-dBtn.appendChild(document.createTextNode('del'));
-li.appendChild(dBtn);
-dBtn.addEventListener('click', deleteNote);
-
-function deleteNote() {
-    li.classList.add("del");
-} //[end] del button
-*/
-
-
-
-/*
-<header>
-  <input id="item" type="text" placeholder="Enter an activity..."/>
-  <button id="add" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
-</header>
-<div class="container">
-  <ul class="todo" id="todo"></ul>
-  <ul class="todo" id="completed"></ul>
-</div>
-*/
-
-
-
-
-
-
-
-/* // prevent-empty-notes
-function inputLength() {
-    return input.value.length;
-}
-
-function newNoteUponClick() {
-    if (inputLength()>0) {
-        addNote();
-    }
-}
-function newNoteUponPress(event) {
-    if (inputLength()>0 && event.which === 13) {
-        'enter'/'return' //13 is enter key's code also event.keyCode === 13
-        addNote();
-    }
-}
-// [end] prevent-empty-notes
-
-
-// user-interaction-scenario
-inputBtn.addEventListener('click',newNoteUponClick);
-
-
-input.addEventListener('keypress',newNoteUponPress);
-//user-interaction-scenario  */
+input.addEventListener("keypress", addListAfterKeypress);
